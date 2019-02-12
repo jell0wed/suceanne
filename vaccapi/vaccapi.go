c15a474dd5f81d156e4092d77946f297196bc1b1
Jeremie Poisson <jepoisso@microsoft.com>
2019-02-11 20:54:51 -0800

    refactored public key

--- vaccapi/vaccapi.go
+++ vaccapi/vaccapi.go
@@ -2,41 +2,44 @@ package vaccapi
 
 import (
 	"github.com/spf13/viper"
-	"strings"
+	//"strings"
 	"crypto/rsa"
 	"crypto/rand"
-	b64 "encoding/base64"
+	log "github.com/sirupsen/logrus"
 )
 
 const clientKey string = "eJUWrzRv34qFSaYk"
 const secret string = "Cyu5jcR4zyK6QEPn1hdIGXB5QIDAQABMA0GC"
-const publicKey []byte = b64.StdEncoding.DecodeString("MIIB/TCCAWYCCQDJ7TMYJFzqYDANBgkqhkiG9w0BAQUFADBCMQswCQYDVQQGEwJjbjEVMBMGA1UEBwwMRGVmYXVsdCBDaXR5MRwwGgYDVQQKDBNEZWZhdWx0IENvbXBhbnkgTHRkMCAXDTE3MDUwOTA1MTkxMFoYDzIxMTcwNDE1MDUxOTEwWjBCMQswCQYDVQQGEwJjbjEVMBMGA1UEBwwMRGVmYXVsdCBDaXR5MRwwGgYDVQQKDBNEZWZhdWx0IENvbXBhbnkgTHRkMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDb8V0OYUGP3Fs63E1gJzJh+7iqeymjFUKJUqSD60nhWReZ+Fg3tZvKKqgNcgl7EGXp1yNifJKUNC/SedFG1IJRh5hBeDMGq0m0RQYDpf9l0umqYURpJ5fmfvH/gjfHe3Eg/NTLm7QEa0a0Il2t3Cyu5jcR4zyK6QEPn1hdIGXB5QIDAQABMA0GCSqGSIb3DQEBBQUAA4GBANhIMT0+IyJa9SU8AEyaWZZmT2KEYrjakuadOvlkn3vFdhpvNpnnXiL+cyWy2oU1Q9MAdCTiOPfXmAQt8zIvP2JC8j6yRTcxJCvBwORDyv/uBtXFxBPEC6MDfzU2gKAaHeeJUWrzRv34qFSaYkYta8canK+PSInylQTjJK9VqmjQ")
 const mainUrlFormat  string = "https://eco-{country}-api.ecovacs.com/v1/private/{country}/{lang}/{deviceId}/{appCode}/{appVersion}/{channel}/{deviceType}"
+var pemPublicKey []byte 
 
 type Vaccapi struct {
+	publicKey []byte
 	deviceId string
 	countryCode string
 	continentCode string
-}
-
-func encrypt(payload string) {
-	payload_b = []byte(payload)
-	ciphertext, err := rsa.SignPKCS1v15(rand.Reader, &publicKey, payload_b)
-
-	if err != nil {
-
-	}
 }
 
 func (api Vaccapi) login() {
 
 }
 
-// Factory method to create a new api instance
-func NewVaccapi() {
-	var api Vaccapi
-	api.baseUrl = "http://" + viper.Get("baseUrl")
-	api.deviceId = viper.Get("deviceId")
+func Encrypt(payload string) {
+	payload_b := []byte(payload)
+	ciphertext, err := rsa.EncryptPKCS1v15(rand.Reader, &pemPublicKey, payload_b)
 
+	if err != nil {
+		log.WithFields(log.Fields{"payload_str": payload}).Error("Unable to encrypt payload string")
+	}
+
+	return ciphertext
+}
+
+// Factory method to create a new api instance
+func NewVaccapi() Vaccapi {
+	pemPublicKey := []byte(viper.Get("publicKey").(string))
+
+	var api Vaccapi
+	api.deviceId = viper.Get("deviceId").(string)
 	return api
 }
