63ef3600391c53f9200f05290086ec2cbf8a0fe8
Jeremie Poisson <jepoisso@microsoft.com>
2019-02-11 21:03:09 -0800

    refactored public key

--- vaccapi/vaccapi.go
+++ vaccapi/vaccapi.go
@@ -2,16 +2,17 @@ package vaccapi
 
 import (
 	"github.com/spf13/viper"
-	//"strings"
 	"crypto/rsa"
 	"crypto/rand"
+	"crypto/x509"
+	"encoding/pem"
 	log "github.com/sirupsen/logrus"
 )
 
 const clientKey string = "eJUWrzRv34qFSaYk"
 const secret string = "Cyu5jcR4zyK6QEPn1hdIGXB5QIDAQABMA0GC"
 const mainUrlFormat  string = "https://eco-{country}-api.ecovacs.com/v1/private/{country}/{lang}/{deviceId}/{appCode}/{appVersion}/{channel}/{deviceType}"
-var pemPublicKey []byte 
+var publicKey *rsa.PublicKey
 
 type Vaccapi struct {
 	publicKey []byte
@@ -24,9 +25,24 @@ func (api Vaccapi) login() {
 
 }
 
-func Encrypt(payload string) {
+func initializePublicKey() *rsa.PublicKey {
+	pemPublicKey := []byte(viper.Get("publicKey").(string))
+	block, _ := pem.Decode(pemPublicKey) // ignore the remaining data
+	if block == nil || block.Type != "PUBLIC KEY" {
+		log.WithFields(log.Fields{"block": block}).Error("Unable to decode PEM public key")
+	}
+
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		log.Error("Unable to parse PKIX public key")
+	}
+	return pub.(*rsa.PublicKey)
+}
+
+func Encrypt(payload string) []byte {
 	payload_b := []byte(payload)
-	ciphertext, err := rsa.EncryptPKCS1v15(rand.Reader, &pemPublicKey, payload_b)
+	
+	ciphertext, err := rsa.EncryptPKCS1v15(rand.Reader, publicKey, payload_b)
 
 	if err != nil {
 		log.WithFields(log.Fields{"payload_str": payload}).Error("Unable to encrypt payload string")
@@ -37,7 +53,7 @@ func Encrypt(payload string) {
 
 // Factory method to create a new api instance
 func NewVaccapi() Vaccapi {
-	pemPublicKey := []byte(viper.Get("publicKey").(string))
+	publicKey = initializePublicKey()
 
 	var api Vaccapi
 	api.deviceId = viper.Get("deviceId").(string)
